8e834425fb2c739cce5a0fd2584b7f472d99be5b
farshad kalhor <farshadt20@yahoo.com>
2020-06-21 17:09:27 +0430

    first init

---
+++ PayDel.Presentation/Controllers/ValuesController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PayDel.Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+        // GET api/val
+        [HttpGet]
+        public async Task<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/val/5
+        [HttpGet("{id}")]
+        public async Task<string> Get(int id)
+        {
+            return "value";
+        }
+        // POST api/values
+        [HttpPost]
+        public async Task<string> Post([FromBody] string value)
+        {
+            return null;
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public async Task<string> Put(int id, [FromBody] string value)
+        {
+            return null;
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public async Task<string> Delete(int id)
+        {
+            return null;
+        }
+
+    }
+}
