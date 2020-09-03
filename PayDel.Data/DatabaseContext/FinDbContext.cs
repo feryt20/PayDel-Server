778cf9f03bb348bd1036287a7e50837bcc4d2f71
farshad kalhor <farshadt20@yahoo.com>
2020-09-03 14:28:58 +0430

    S36-E17

---
+++ PayDel.Data/DatabaseContext/FinDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PayDel.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PayDel.Data.DatabaseContext
+{
+    public class FinDbContext : DbContext
+    {
+        public FinDbContext()
+        {
+        }
+        public FinDbContext(DbContextOptions<FinDbContext> opt) : base(opt)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+        {
+            optionBuilder
+                .UseSqlServer(@"Server=(local);Database=PayDel_FinDb;User Id=sa;Password=fery;Trusted_Connection=True;MultipleActiveResultSets=True;");
+        }
+
+        public DbSet<Factor> Factors { get; set; }
+        public DbSet<Entry> Entries { get; set; }
+    }
+}
